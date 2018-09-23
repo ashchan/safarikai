d2f1ff3784ce8cd50b2c2180113c737de849c6db
Aaron Lee <aaron@onefatgiraffe.com>
2018-09-23 17:10:02 +0900

    Main app instructions and Safari Extension shortcut  botton

--- Safarikai/ViewController.swift
+++ Safarikai/ViewController.swift
@@ -20,5 +20,27 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
+    
+    @IBAction func openSafariExtPrefsPressed(_ sender: Any) {
+        let myAppleScript = """
+            tell application "Safari" to activate
+            tell application "System Events" to tell process "Safari"
+                keystroke "," using command down
+                tell window 1
+                    click button "Extensions" of tool bar 1
+                    activate "Extensions"
+                end tell
+            end tell
+        """
+        var error: NSDictionary?
+        if let scriptObject = NSAppleScript(source: myAppleScript) {
+            if let output: NSAppleEventDescriptor = scriptObject.executeAndReturnError(
+                &error) {
+                print(output.stringValue)
+            } else if (error != nil) {
+                print("error: \(error)")
+            }
+        }
+    }
 }
 
