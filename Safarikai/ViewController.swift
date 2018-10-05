639449fa43f4390d811aa969a5a1b5cf1ba79724
James Chen <james@ashchan.com>
2018-10-05 13:00:04 +0900

    Replace install button with guide form the main app
    The Apple script doesn't always work as expected.

--- Safarikai/ViewController.swift
+++ Safarikai/ViewController.swift
@@ -9,23 +9,4 @@
 import Cocoa
 
 class ViewController: NSViewController {
-    @IBAction func openSafariExtPrefsPressed(_ sender: Any) {
-        let myAppleScript = """
-            tell application "Safari" to activate
-            tell application "System Events" to tell process "Safari"
-                keystroke "," using command down
-                tell window 1
-                    click button "Extensions" of tool bar 1
-                    activate "Extensions"
-                end tell
-            end tell
-        """
-        var error: NSDictionary?
-        if let scriptObject = NSAppleScript(source: myAppleScript) {
-            scriptObject.executeAndReturnError(&error)
-            if let error = error {
-                print("error: \(error)")
-            }
-        }
-    }
 }
